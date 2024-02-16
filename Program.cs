0006ee6a9a51a1d8bf89d129e9a90296f68e12e3
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-02-16 12:59:22 -0600

    code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
-﻿namespace HelloWorld_Jackson_Lydia
+﻿// system namespace needed to use the console class
+using System;
+
+// my collection of classes for this project
+
+namespace HelloWorld_Jackson_Lydia
 {
+    // main class for our program
     internal class Program
     {
+        // the main method is executed when the application starts
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // asks the user for first name
+            Console.WriteLine("What is your first name?");
+            // assigns user input to firstName variable
+            string firstName = Console.ReadLine();
+            // asks the user for last name
+            Console.WriteLine("What is your last name?");
+            // assigns user input to lastName variable
+            string lastName = Console.ReadLine(); 
+
+            // prints a welcome message with firstName and lastName
+            Console.WriteLine($"Hello, {firstName} {lastName}!");
         }
     }
 }
